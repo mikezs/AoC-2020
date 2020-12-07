0d3abf2ae2ac20e7b23f2fb86c18eac54c35f624
Mike Bignell <mike@mbignell.com>
2020-12-07 09:17:03 +0000

    Day 7 part 1

--- Sources/AoC2020Kit/Day7.swift
+++ Sources/AoC2020Kit/Day7.swift
@@ -1,17 +1,41 @@
 import Foundation
 
 public final class Day7: Day {
-    private let input: [String]
+    typealias BagColor = String
+    
+    private let input: [BagColor: [BagColor: Int]]
     
     public init(input: String) {
-        self.input = input.trimmedLines
+        self.input = input.trimmedLines.reduce([BagColor: [BagColor: Int]](), { dict, rule in
+            let parts = rule.components(separatedBy: " bags contain ")
+            let matches = parts[1].matches(for: "([0-9]+) (.*?) bag")
+            var dict = dict
+            
+            dict[parts[0]] = matches.reduce([BagColor: Int]()) {
+                var dict = $0
+                dict[$1[1]] = Int($1[0])!
+                return dict
+            }
+            
+            return dict
+        })
     }
 
     public func part1() -> Int {
-        0
+        let search = "shiny gold"
+        
+        return input.filter { bag($0.key, contains: search) }.count
     }
 
     public func part2() -> Int {
         0
+    }
+    
+    private func bag(_ bag: BagColor, contains search: BagColor) -> Bool {
+        if input[bag]?.keys.contains(search) == true {
+            return true
+        }
+        
+        return input[bag]?.contains { self.bag($0.key, contains: search) } == true
     }
 }
--- Tests/AoC2020KitTests/AoC_2020Tests.swift
+++ Tests/AoC2020KitTests/AoC_2020Tests.swift
@@ -166,10 +166,19 @@ b
     }
     
     let day7Input = """
+light red bags contain 1 bright white bag, 2 muted yellow bags.
+dark orange bags contain 3 bright white bags, 4 muted yellow bags.
+bright white bags contain 1 shiny gold bag.
+muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.
+shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.
+dark olive bags contain 3 faded blue bags, 4 dotted black bags.
+vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
+faded blue bags contain no other bags.
+dotted black bags contain no other bags.
 """
         
         func testDay7Part1() {
-            XCTAssertEqual(Day7(input: day7Input).part1(), 0)
+            XCTAssertEqual(Day7(input: day7Input).part1(), 4)
         }
         
         func testDay7Part2() {
