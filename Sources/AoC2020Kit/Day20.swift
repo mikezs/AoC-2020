d13af6d3cc2822923c16f5df1c9cd45de9399e20
Mike Bignell <mike@mbignell.com>
2020-12-21 16:27:30 +0000

    More Day 20

--- Sources/AoC2020Kit/Day20.swift
+++ Sources/AoC2020Kit/Day20.swift
@@ -2,16 +2,40 @@ import Foundation
 
 public final class Day20: Day {
     public final class Tile {
+        enum Side {
+            case left
+            case right
+            case top
+            case bottom
+        }
+
         let number: Int
         private(set) var leftConnection: Tile?
         private(set) var rightConnection: Tile?
         private(set) var topConnection: Tile?
         private(set) var bottomConnection: Tile?
+        let contents: [[Bool]]
+        let sides: [Side: [Bool]]
 
         init(_ string: String) {
             let lines = string.components(separatedBy: .newlines)
 
             number = Int(lines[0].matches(for: "Tile ([0-9]+):")[0][0])!
+
+            var contents = [[Bool]]()
+
+            for index in 1..<lines.count {
+                contents.append(lines[index].reduce([Bool](), { $0 + [$1 == "#"] }))
+            }
+
+            self.contents = contents
+
+            var sides = [Side: [Bool]]()
+            sides[.left] = contents.reduce([Bool](), { $0 + [$1[0]] })
+            sides[.right] = contents.reduce([Bool](), { $0 + [$1[$1.count - 1]] })
+            sides[.top] = contents[0]
+            sides[.bottom] = contents[contents.count - 1]
+            self.sides = sides
         }
 
         public var connections: [Tile] {
@@ -30,7 +54,14 @@ public final class Day20: Day {
     }
 
     private func connect(tiles: [Tile]) {
+        //let sideLength = Int(Double(tiles.count).squareRoot())
+        var sides = [Int: (Tile.Side, Tile)]()
 
+        for tile in tiles {
+            for (side, values) in tile.sides {
+                //sides[]
+            }
+        }
     }
 
     public func part1() -> Int {
